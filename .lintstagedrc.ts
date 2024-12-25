c48616728fe8f7a016cbcd108eb7f77863f2f279
John Barry <john.olivierb@gmail.com>
2024-12-25 08:59:33 -0500

    Remove .lintstagedrc.ts

--- .lintstagedrc.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'lint-staged'
-
-const config: Config = {
-  '**/*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix'],
-  '**/*.{json,css,scss,md}': ['prettier --write'],
-}
-
-export default config
