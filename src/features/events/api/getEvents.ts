f7b1936e47b5264bc3a66d526927c3dbd3dd1e6e
John Barry <john.olivierb@gmail.com>
2025-01-18 13:29:31 -0500

    Parallax text effect on event showcase section

--- src/features/events/api/getEvents.ts
+++ src/features/events/api/getEvents.ts
@@ -15,3 +15,13 @@ export async function getEventById(id: string) {
 export async function getEventBySlug(slug: string) {
   return await supabase.from('events').select('*').eq('slug', slug).single()
 }
+
+export async function getUpcomingEvents() {
+  return await supabase
+    .from('events')
+    .select('*')
+    .gte('date', new Date().toISOString())
+    .order('date', { ascending: true })
+    .limit(1)
+    .single()
+}
--- src/features/home/components/EventShowcase/EventShowcase.tsx
+++ src/features/home/components/EventShowcase/EventShowcase.tsx
@@ -3,12 +3,12 @@
 import { motion, useScroll, useTransform } from 'motion/react'
 import { useEffect, useRef } from 'react'
 import { useState } from 'react'
+import { VelocityScroll } from '@components/ui/scroll-based-velocity'
 import { EventPoster } from '@events/components/EventGallery/EventPoster'
-import { useEvents } from '@events/hooks/useEvents'
+import { useUpcomingEvent } from '@events/hooks/useEvents'
 
 export function EventShowcase() {
-  const { events } = useEvents(4)
-  const upcomingEvent = events[0]
+  const { upcomingEvent } = useUpcomingEvent()
   const sectionRef = useRef<HTMLDivElement>(null)
   const [isHovered, setIsHovered] = useState(false)
 
@@ -29,19 +29,28 @@ export function EventShowcase() {
     return () => unsubscribe()
   }, [scrollYProgress])
 
-  const posterY = useTransform(scrollYProgress, [0, 0.4, 1], [500, 0, -50])
+  const posterY = useTransform(scrollYProgress, [0, 0.5, 1], [100, 0, -50])
   const posterRotation = useTransform(
     scrollYProgress,
     [0, 0.5, 1],
-    [45, 10, -10],
+    [25, -5, -15],
   )
-  const posterScale = useTransform(scrollYProgress, [0, 0.5, 1], [0.5, 1, 1.1])
-  const textY = useTransform(scrollYProgress, [0.3, 0.8, 1], [-100, 0, -20])
+  const posterScale = useTransform(scrollYProgress, [0, 0.5, 1], [0.9, 1, 0.8])
+
+  const textY = useTransform(scrollYProgress, [0.3, 0.5, 1], [-100, 0, -20])
   const textOpacity = useTransform(scrollYProgress, [0, 0.5], [0, 1])
   const textScale = useTransform(scrollYProgress, [0, 0.5, 1], [1.3, 1, 0.9])
 
+  const divOpacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0])
+
   return (
-    <motion.div ref={sectionRef} className="container py-24">
+    <motion.div
+      style={{
+        opacity: divOpacity,
+      }}
+      ref={sectionRef}
+      className={upcomingEvent ? 'container pb-24 pt-12' : 'py-0'}
+    >
       <div className="relative flex items-center justify-center">
         <motion.h1
           style={{
@@ -49,22 +58,35 @@ export function EventShowcase() {
             opacity: textOpacity,
             scale: textScale,
           }}
-          className="pointer-events-none absolute z-10 text-6xl font-bold text-foreground"
+          className="pointer-events-none absolute z-0 flex h-full items-center justify-center text-6xl font-black uppercase tracking-tight text-foreground [mask-image:radial-gradient(600px_circle_at_center,white,transparent)]"
         >
-          Prochain événement
+          <VelocityScroll defaultVelocity={0.5}>
+            Prochain événement Inscription ouverte
+          </VelocityScroll>
+          ;
         </motion.h1>
-        <motion.div
-          className="relative z-0 flex w-full items-center justify-center"
-          style={{ y: posterY, rotate: posterRotation, scale: posterScale }}
-        >
-          {upcomingEvent && (
-            <EventPoster
-              size="md"
-              event={upcomingEvent}
-              isHovered={isHovered}
-            />
-          )}
-        </motion.div>
+        <div className="relative inset-0 h-full w-full">
+          <motion.div
+            className="relative z-0 flex w-full items-center justify-center"
+            style={{
+              y: posterY,
+              rotate: posterRotation,
+              scale: posterScale,
+            }}
+          >
+            {upcomingEvent && (
+              <EventPoster
+                tiltProps={{
+                  tiltMaxAngleX: 16,
+                  tiltMaxAngleY: 16,
+                }}
+                size="md"
+                event={upcomingEvent}
+                isHovered={isHovered}
+              />
+            )}
+          </motion.div>
+        </div>
       </div>
     </motion.div>
   )
